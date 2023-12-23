0e6e106d6791e6edbb2ebb6d2df46efd85c001e3
NevenQP <phuongtt.bi12-353@st.usth.edu.vn>
2023-12-24 05:00:45 +0700

    data

--- Teacher/course.php
+++ Teacher/course.php
@@ -131,7 +131,7 @@
    <div class="container mt-4">
         <?php
             if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
+                while ($row = $result->fetch_assoc()) {}}
         ?>
         <div class="courseDetail">
             <a class="link_course" href="../Teacher/coursedetail.php">Machine Learning and Data Mining</a>
--- db_conn.php
+++ db_conn.php
@@ -1,9 +1,9 @@
 <?php
     $servername = "127.0.0.1";
     $username = "root";
-    $password = "";
+    $password = "123456";
 
-    $db_name = "school";
+    $db_name = "project";
 
     $conn = mysqli_connect($servername, $username, $password, $db_name);
 
