781577604a29329d2a92fc943e65dd8aceba1c1f
bethanytran <125539480+bethanytran@users.noreply.github.com>
2023-12-24 23:59:56 +0700

    update admin nav

--- admin/admin_profile.php
+++ admin/admin_profile.php
@@ -8,8 +8,8 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         <link rel="stylesheet" href="../admin/admin_profile.css">
-        <link rel="stylesheet" href="../Teacher/navbar.css">
-        <link rel="stylesheet" href="../Teacher/footer.css">
+        <link rel="stylesheet" href="../admin/navbar.css">
+        <link rel="stylesheet" href="../admin/footer.css">
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
         <!-- JQuery -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
--- admin/curriculum.php
+++ admin/curriculum.php
@@ -8,8 +8,8 @@
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="../admin/curriculum.css">
-    <link rel="stylesheet" href="../Teacher/navbar.css">
-    <link rel="stylesheet" href="../Teacher/footer.css">
+    <link rel="stylesheet" href="../admin/navbar.css">
+    <link rel="stylesheet" href="../admin/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="./FontAwesome.Pro.6.4.2/css/all.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
