32df53e167decaadc647b3edd5cf9dd341737bc5
bethanytran <125539480+bethanytran@users.noreply.github.com>
2023-12-28 00:27:29 +0700

    update page nav

--- Teacher/attendance.php
+++ Teacher/attendance.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="../Teacher/studenList.css">
+    <link rel="stylesheet" href="../Teacher/studentList.css">
     <link rel="stylesheet" href="../Teacher/navbar.css">
     <link rel="stylesheet" href="../Teacher/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
@@ -412,6 +412,34 @@
               
             </tbody>
           </table>
+    </div>
+    <div class="container mt-4">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination page-custom">
+          <li class="page-item" style="padding-right: 10px;">
+            <a class="page-link custom-color" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item" style="padding-right: 10px;">
+            <a class="page-link custom-color" href="#">1</a>
+          </li>
+          <li class="page-item" style="padding-right: 10px;">
+            <a class="page-link custom-color" href="#">2</a>
+          </li>
+          <li class="page-item" style="padding-right: 10px;">
+            <a class="page-link custom-color" href="#">...</a>
+          </li>
+          <li class="page-item" style="padding-right: 10px;">
+            <a class="page-link custom-color" href="#">4</a>
+          </li>
+          <li class="page-item">
+            <a class="page-link custom-color" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
     
     <!-- Footer -->
