17f88f531dd650a8e6ad78f39c1adcc68474403d
minchargi <phuongdlp.bi12-358@st.usth.edu.vn>
2023-12-30 22:28:15 +0700

    staff add new dept.

--- staff/courses.php
+++ staff/courses.php
@@ -22,11 +22,16 @@
       </div>
       <div class="container mt-4">
         <div class="btn-container">
-          <button class="custom-button" data-bs-toggle="modal" data-bs-target="#courseM">
-            <span class="button-content gap-4">
-                Add
-            </span>
-        </button>
+            <button class="custom-button" data-bs-toggle="modal" data-bs-target="#courseM">
+                <span class="button-content gap-4">
+                    Add
+                </span>
+            </button>
+            <button class="custom-button" data-bs-toggle="modal" data-bs-target="#courseDeptM">
+                <span class="button-content gap-4">
+                    Add Dept
+                </span>
+            </button>
         </div>
       </div>
       <!-- Add Course Modal-->
@@ -70,6 +75,45 @@
                               </div>
                             </div>
                             
+                            <button type="submit" class="btn btn-custom" name="submit">Add</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Add Course Dept Modal-->
+        <div class="modal fade" id="courseDeptM" tabindex="-1" role="dialog" aria-labelledby="courseDeptMLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="courseDeptMLabel">Add New Intake</h5>
+                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Form for adding new intake -->
+                        <form id="courseDeptForm" action="../staff/courseM.php" method="post">
+                            <div class="form-group">
+                              <label for="intakeChoose">Intake:</label>
+                              <select class="form-control" id="intakeChoose" name="intakeChoose" required>
+                                  <option value="" selected disabled>Select an option</option>
+                                  <option value="dept1">Department of Information and Communication Technology</option>
+                                  <option value="dept2">Department of Space and Applications</option>
+                                  <option value="dept3">Department of Energy</option>
+                                  <option value="dept4">Department of Life Science</option>
+                                  <option value="dept5">Department of Water - Environment - Oceanography</option>
+                              </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="newDeptName">Department Name:</label>
+                                <input type="text" class="form-control" id="newDeptName" name="newDeptName" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="newMajor">New Major:</label>
+                                <input type="text" class="form-control" id="newMajor" name="newMajor" required>
+                            </div>
+                              
                             <button type="submit" class="btn btn-custom" name="submit">Add</button>
                         </form>
                     </div>
