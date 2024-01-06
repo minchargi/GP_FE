983ff9f579f1eb95f2dc97b7094aa66caf4ea7eb
bethanytran <125539480+bethanytran@users.noreply.github.com>
2024-01-06 22:47:17 +0700

    staff update

--- admin/staff_List.php
+++ admin/staff_List.php
@@ -79,6 +79,11 @@
                     Add Staff
                 </span>
             </button>
+            <button class="custom-button" data-bs-toggle="modal" data-bs-target="#importListsModal">
+                <span class="button-content gap-4">
+                    Import
+                </span>
+            </button>
         </div>
     </div>
 
@@ -132,6 +137,29 @@
                             <input type="email" class="form-control" id="email" name="email" required>
                         </div>
                         <button type="submit" class="btn btn-custom" name="submit">Add</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+     <!-- Import Lists Modal -->
+     <div class="modal fade" id="importListsModal" tabindex="-1" role="dialog" aria-labelledby="importListsModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="importListsModalLabel">Import Lists</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- Form for importing lists -->
+                    <form id="importListsForm">
+                        <div class="form-group">
+                            <label for="fileInput">Import Lists:</label>
+                            <input type="file" class="form-control" id="fileInput" required>
+                        </div>
+                        <button type="button" class="btn btn-custom" onclick="importLists()">Import</button>
                     </form>
                 </div>
             </div>
