361464c762a7de17e8e69c10e7940389ba7a09ff
minchargi <phuongdlp.bi12-358@st.usth.edu.vn>
2024-01-08 22:50:25 +0700

    retake optional fix

--- Teacher/course.php
+++ Teacher/course.php
@@ -31,7 +31,55 @@
 <body>
     
     <!-- Navbar-->
-    <?php include '../navbar/teachernav.php'; ?>
+    <header>
+          <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+              <img class="logo" src="../images/logo-moi_2.svg" alt="logo" width="90" height="50.78">
+            </a>
+            <div class="container-fluid">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                  <li class="nav-item">
+                    <a class="nav-link" href="../Teacher/announcement.php"><i class="fas fa-home"></i> Announcement</a>
+                  </li>
+                  <li class="nav-item ">
+                    <a class="nav-link" href="../Teacher/courseList.php"> 
+                      <i class="fas fa-book"></i> Course
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="../Teacher/contact.php"><i class="fa-solid fa-phone"></i> Contact</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-calendar"></i> Timetable
+                      </a>
+                      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="https://usth.edu.vn/en/timetableo-of-year-2-program-in-information-and-communication-technology-5959/">Second Year</a></li>
+                        <li><a class="dropdown-item" href="https://usth.edu.vn/en/timetable-of-year-3-ict-5961/">Third Year</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#"><i class="far fa-question-circle"></i> </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <i class="fas fa-user-circle"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="../Teacher/profile.php">Account</a></li>
+                      <li><a class="dropdown-item" href="../sign-in/logout.php">Logout</a></li>
+                    </ul>
+                  </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </nav>
+      </header>
 
     </div>
     <div class="container mt-4">
@@ -99,8 +147,42 @@
         ?>
    </div>
    <!-- Footer -->
-   <?php include '../footer/footer.php'; ?>
+   <footer>
+        <div class="row mt-4">
+            <div class="col-md-4 infor">
+                <img class="white-logo" src="../images/white_logo.png">
+                <!--Address-->
+                <p class="detail"> <i class="fa-solid fa-house icon-footer"></i>
+                    Address
+                </p>
+                <p class="text-detail"> A21 building, Vietnam Academy of Science and Technology, 18 Hoang Quoc Viet, Cau Giay, Hanoi</p>
+                <hr class="divider">
 
+                <!--Phone-->
+                <p class="detail"><i class="fa-solid fa-phone icon-footer"></i>
+                Phone</p>
+                <p class="text-detail"> +84-24 37 91 69 60</p>
+                <hr class="divider">
+                
+                <!--Email-->
+                <p class="detail"> <i class="fa-solid fa-envelope icon-footer"></i>
+                    Email
+                </p>
+                <p class="text-detail">officeusth@usth.edu.vn</p>
+                <hr class="divider">
+                
+                <!--Contact-->
+                <p class="detail"> <i class="fa-solid fa-envelope icon-footer"></i>
+                    Contact
+                <p class="text-detail">webmaster@usth.edu.vn</p>
+                <hr class="divider">
+                
+            </div>
+            <div class="copy-right  py-2 mt-4">
+                <p class="ft-text">&copy; 2021 <span class="web-site">usth.edu.vn</span>. All Rights reserved</p>
+            </div>
+        </div>
+      </footer>
 
   </body>
 </html>
--- staff/students.php
+++ staff/students.php
@@ -300,6 +300,7 @@
             </div>
           </div>
          </div>
+      </div>
 
        <script>
         function toggleAccordion(btn) {
